3fed1c80c22e9bf8946022e51fb4159ce74ff566
ris-tlp <omarkhantlp@gmail.com>
2019-05-06 00:09:55 +0300

    init

---
+++ bot.py
@@ -0,0 +1,28 @@
+import discord
+from discord.ext import commands
+
+TOKEN = ""
+prefix = ("$")
+bot = commands.Bot(command_prefix = prefix)
+
+extensions = ['cogs.fun']
+
+@bot.event
+async def on_ready():
+    await bot.change_presence(game = discord.Game(name = "with itself"))
+    print("Bot running.")
+
+@bot.command(pass_context = True)
+async def close(ctx):
+    await bot.say("ciao")
+    await bot.logout()
+
+if __name__ == '__main__':
+    for extension in extensions:
+        try:
+            bot.load_extension(extension)
+            print("{} loaded".format(extension))
+        except Exception as error:
+            print("{} cannot be loaded. [{}]".format(extension, error))
+
+bot.run(TOKEN)
---
+++ cogs/fun.py
@@ -0,0 +1,33 @@
+import discord
+from discord.ext import commands
+import random
+
+class Fun:
+    def __init__(self, bot):
+        self.bot = bot
+
+    async def on_message_delete(self, message):
+        await self.bot.send_message(message.channel, "Message deleted")
+
+    @commands.command()
+    async def ping(self):
+        await self.bot.say("no")
+
+    @commands.command(pass_context = True, name = '8ball')
+    async def ball(self, ctx):
+        responses = ["As I see it, yes",
+                    "Ask again later",
+                    "Better not tell you now",
+                    "Don't count on it",
+                    "It is certain",
+                    "Most likely",
+                    "Outlook good",
+                    "Outlook not so good",
+                    "Without a doubt",
+                    "Doubtful"]
+
+        await self.bot.say(random.choice(responses) + ", " + ctx.message.author.mention)
+
+
+def setup(bot):
+    bot.add_cog(Fun(bot))
